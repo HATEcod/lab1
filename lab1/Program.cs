f86f28bf80a9d7215cf6698b50a7719ba2f7c3ae
HATEcod <hate13123456789@gmail.com>
2021-05-12 11:18:23 +0300

    Initial commit

---
+++ lab1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab1
+{
+    public class Lab1
+    {
+        public int counter_exceptions_critical{
+            get;
+            private set;
+        }
+
+        public int regular_exceptions_counter{
+             get;
+            private set;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public bool is_critical(Exception exception)
+        {
+            var critical_Exceptions = new List<Type>()
+              {
+                typeof(DivideByZeroException),
+                typeof(OutOfMemoryException),
+                typeof(StackOverflowException),
+                typeof(InsufficientMemoryException),
+                typeof(InsufficientExecutionStackException)
+              };
+            return critical_Exceptions.Contains(exception.GetType());
+        }
+
+        public void CountExceptions(Exception exception)
+        {
+            if (is_critical(exception))
+            {
+                counter_exceptions_critical += 1;
+            }
+            else
+            {
+                regular_exceptions_counter += 1;
+            }
+        }
+    }
+}
---
+++ lab1/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using lab1;
+using NUnit.Framework;
+
+namespace Project1.UnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase(typeof(DivideByZeroException), true)]
+        [TestCase(typeof(OutOfMemoryException), true)]
+        [TestCase(typeof(StackOverflowException), true)]
+        [TestCase(typeof(InsufficientMemoryException), true)]
+        [TestCase(typeof(InsufficientExecutionStackException), true)]
+
+        [TestCase(typeof(ArgumentNullException), false)]
+        [TestCase(typeof(ArgumentOutOfRangeException), false)]
+        [TestCase(typeof(NullReferenceException), false)]
+        [TestCase(typeof(AccessViolationException), false)]
+        [TestCase(typeof(IndexOutOfRangeException), false)]
+        [TestCase(typeof(InvalidOperationException), false)]
+        public void Check_if_is_critical_criticality_Correct(Type exceptionType, bool expectedResult)
+        {
+          // arrange: об'єктів, тобто створення і налаштування.
+          var instance = (Exception)Activator.CreateInstance(exceptionType);
+            try
+            {
+                // act: на об'єкт.
+                throw instance;
+            }
+            catch (Exception e)
+            {
+                // assert: про очікуваний результат.
+                Assert.AreEqual(expectedResult, new Lab1().is_critical(e));
+                return;
+            }
+        }
+
+        [Test]
+        public void Count_exceptions_counter_values_correct()
+        {
+            // arrange: об'єктів, тобто створення і налаштування.
+            var criticalexceptions = new List<Type>()
+                  {
+                    typeof(DivideByZeroException),
+                    typeof(OutOfMemoryException),
+                    typeof(StackOverflowException),
+                    typeof(InsufficientMemoryException),
+                    typeof(InsufficientExecutionStackException)};
+
+            var notcriticalexceptions = new List<Type>()
+                  { typeof(ArgumentNullException),
+                    typeof(ArgumentOutOfRangeException),
+                    typeof(NullReferenceException),
+                    typeof(AccessViolationException),
+                    typeof(IndexOutOfRangeException),
+                    typeof(InvalidOperationException)};
+
+            var lab1 = new Lab1();
+
+            // act: на об'єкт.
+            foreach (var item in criticalexceptions)
+            {
+                var instance = (Exception)Activator.CreateInstance(item);
+                lab1.CountExceptions(instance);
+            }
+            foreach (var item in notcriticalexceptions)
+            {
+                var instance = (Exception)Activator.CreateInstance(item);
+                lab1.CountExceptions(instance);
+            }
+
+            // assert: про очікуваний результат.
+            Assert.AreEqual(lab1.counter_exceptions_critical,criticalexceptions.Count);
+            Assert.AreEqual(lab1.regular_exceptions_counter,notcriticalexceptions.Count);
+        }
+
+        [Test]
+        public void CountExceptions_InitCounters_Zero()
+        {
+            // arrange: об'єктів, тобто створення і налаштування.
+            var lab1 = new Lab1();
+
+            // assert: про очікуваний результат.
+            Assert.AreEqual(lab1.counter_exceptions_critical,0);
+            Assert.AreEqual(lab1.regular_exceptions_counter,0);
+        }
+    }
+}
